5e57663ad5a1046da905765fe3c1bc0396cab2de
Sami Habbaba <sami.software@hotmail.com>
2021-09-28 10:07:50 +0300

    captain page finished

--- src/app/shared/forms/add-captain/add-captain.component.ts
+++ src/app/shared/forms/add-captain/add-captain.component.ts
@@ -1,15 +1,105 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { Dialog } from 'primeng/dialog';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/services/data/data.service';
+import { FormService } from 'src/app/services/form-service/form.service';
 
 @Component({
   selector: 'app-add-captain',
   templateUrl: './add-captain.component.html',
-  styleUrls: ['./add-captain.component.scss']
+  styleUrls: ['./add-captain.component.scss'],
 })
 export class AddCaptainComponent implements OnInit {
+  objectSubscriber$: Subscription;
+  submitSubscriber$: Subscription;
+  formValidationSubscriber$: Subscription;
+  form: FormGroup;
 
-  constructor() { }
+  dropdownOptions: any[];
 
-  ngOnInit(): void {
+  constructor(
+    private dataService: DataService,
+    private formService: FormService,
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private dialogRef: Dialog
+  ) {
+    this.dialogRef.onShow.subscribe(() => {
+      this.dropdownOptions = this.dataService.countries;
+      this.initializeForm();
+      this.loadSubscriptions();
+    });
+    this.dialogRef.onHide.subscribe(() => {
+      this.destroySubscription();
+    });
   }
 
+  ngOnInit() {}
+
+  loadSubscriptions() {
+    this.objectSubscriber$ = this.formService
+      .getFormObject()
+      .subscribe((value) => {
+        console.log(value);
+      });
+
+    this.submitSubscriber$ = this.formService
+      .getSubmitSubject()
+      .subscribe((value) => {
+        if (value === 'submit') {
+          this.submitForm();
+        }
+      });
+
+    this.formValidationSubscriber$ = this.formService.listenToValueChanges(
+      this.form
+    );
+  }
+
+  destroySubscription() {
+    this.objectSubscriber$.unsubscribe();
+    this.submitSubscriber$.unsubscribe();
+    this.formValidationSubscriber$.unsubscribe();
+  }
+
+  submitForm() {
+    let obj = this.form.getRawValue();
+    this.dataService.addNewCaptain(obj).subscribe((response) => {
+      this.formService.triggerRefresh();
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Yeni kaptan başarıyla eklendi',
+      });
+    });
+  }
+
+  initializeForm() {
+    this.form = this.fb.group({
+      firstName: new FormControl(null, [Validators.required]),
+      lastName: new FormControl(null, [Validators.required]),
+      title: new FormControl(null, []),
+      telephone: new FormControl(null, []),
+      country: new FormControl(null, []),
+      district: new FormControl(null, []),
+      address: new FormControl(null, []),
+      webLink: new FormControl(null, []),
+      email: new FormControl(null, []),
+      isLocal: new FormControl(false, []),
+      isGuidline: new FormControl(false, []),
+    });
+  }
+
+  checkValidity(formControl: string) {
+    return (
+      this.form.get(formControl)?.touched && this.form.get(formControl)?.invalid
+    );
+  }
 }
--- src/app/shared/forms/captain-details/captain-details.component.ts
+++ src/app/shared/forms/captain-details/captain-details.component.ts
@@ -1,15 +1,111 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { Dialog } from 'primeng/dialog';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/services/data/data.service';
+import { FormService } from 'src/app/services/form-service/form.service';
 
 @Component({
   selector: 'app-captain-details',
   templateUrl: './captain-details.component.html',
-  styleUrls: ['./captain-details.component.scss']
+  styleUrls: ['./captain-details.component.scss'],
 })
 export class CaptainDetailsComponent implements OnInit {
+  objectSubscriber$: Subscription;
+  submitSubscriber$: Subscription;
+  formValidationSubscriber$: Subscription;
+  form: FormGroup;
+  objReceived: any;
 
-  constructor() { }
+  dropdownOptions: any[];
 
-  ngOnInit(): void {
+  constructor(
+    private dataService: DataService,
+    private formService: FormService,
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private dialogRef: Dialog
+  ) {
+    this.dialogRef.onShow.subscribe(() => {
+      this.dropdownOptions = this.dataService.countries;
+      this.loadSubscriptions();
+    });
+    this.dialogRef.onHide.subscribe(() => {
+      this.destroySubscription();
+    });
   }
 
+  ngOnInit() {}
+
+  loadSubscriptions() {
+    this.objectSubscriber$ = this.formService
+      .getFormObject()
+      .subscribe((value) => {
+        this.objReceived = value;
+      });
+
+    this.initializeForm();
+    this.submitSubscriber$ = this.formService
+      .getSubmitSubject()
+      .subscribe((value) => {
+        if (value === 'submit') {
+          this.submitForm();
+        }
+      });
+
+    this.formValidationSubscriber$ = this.formService.listenToValueChanges(
+      this.form
+    );
+  }
+
+  destroySubscription() {
+    this.objectSubscriber$.unsubscribe();
+    this.submitSubscriber$.unsubscribe();
+    this.formValidationSubscriber$.unsubscribe();
+  }
+
+  submitForm() {
+    let obj = this.form.getRawValue();
+    obj.id = this.objReceived.id;
+    this.dataService.updateCaptain(obj).subscribe((response) => {
+      this.formService.triggerRefresh();
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Kaptan başarıyla güncellendi',
+      });
+    });
+  }
+
+  initializeForm() {
+    this.form = this.fb.group({
+      firstName: new FormControl(this.objReceived.firstName, [
+        Validators.required,
+      ]),
+      lastName: new FormControl(this.objReceived.lastName, [
+        Validators.required,
+      ]),
+      title: new FormControl(this.objReceived.title, []),
+      telephone: new FormControl(this.objReceived.telephone, []),
+      country: new FormControl(this.objReceived.country, []),
+      district: new FormControl(this.objReceived.district, []),
+      address: new FormControl(this.objReceived.address, []),
+      webLink: new FormControl(this.objReceived.webLink, []),
+      email: new FormControl(this.objReceived.email, []),
+      isLocal: new FormControl(this.objReceived.isLocal, []),
+      isGuidline: new FormControl(this.objReceived.isGuidline, []),
+    });
+  }
+
+  checkValidity(formControl: string) {
+    return (
+      this.form.get(formControl)?.touched && this.form.get(formControl)?.invalid
+    );
+  }
 }
