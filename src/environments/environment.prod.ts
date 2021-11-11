5a73bc4a56a0db4263b1ee58503cb76b3ac29a95
AmroMbd <37118920+AmroMbd@users.noreply.github.com>
2021-11-11 12:34:28 +0200

    official url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   isMockEnabled: false, // You have to switch this, when your real back-end is done
   authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
-  apiUrl: 'https://api.lios3.xyz/api/v1/',
+  // apiUrl: 'https://api.lios3.xyz/api/v1/',
+  apiUrl: 'https://api.lios3.limanlar.gov.ct.tr/api/v1/',
 };
